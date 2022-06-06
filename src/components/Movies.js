70b12694f5b88766dd7296061656fd8520d05ed3
Michelle <clairsin@gmail.com>
2022-06-05 23:35:49 -0400

    commit tree aa7c2d5fcca16adcd34d08d7a46ccb2729308685

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,31 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+
+
+  return (
+    <div>
+      <h1>Movies Page</h1>
+        {movies.map((movie) => { 
+          return (
+            <div key={movie.title}>
+            Name: {movie.title}
+            <br></br>
+            Time: {movie.time}
+            <br></br>
+            Genres: 
+            <ul>
+            {movie.genres.map((genre) => {
+              return (
+                  <li key={genre}>
+                    {genre}
+                  </li>
+              )
+            })}
+            </ul>
+          </div>)
+        })}
+    </div>);
 }
 
 export default Movies;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,17 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <NavLink exact to="/movies">Movies</NavLink>
+      <br></br>
+      <NavLink exact to="/directors">Directors</NavLink>
+      <br></br>
+      <NavLink exact to="/actors">Actors</NavLink>
+      <br></br>
+      <NavLink exact to="/">Home</NavLink>
+    </div>
+  )
 }
 
 export default NavBar;
